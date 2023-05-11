c4698c8854f305cb2444ad9b067c88ec17fcb35c
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-05-11 17:34:40 -0300

    svg

--- anothertestapp/resources/views/home.blade.php
+++ anothertestapp/resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 @section('title', 'Inicio | CKFM')
 @section('content')
+@include('include.nav')
 @include('include.header')
 {{-- @include('include.footer') --}}
 @endsection
---
+++ anothertestapp/resources/views/include/nav.blade.php
@@ -0,0 +1,68 @@
+
+<nav class="glass z-10">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+      <!-- Logo -->
+      <div class="flex-shrink-0">
+        <img class="mt-8 w-[150px]" src="../resources/img/logowhite.svg" alt="Logo">
+      </div>
+      <!-- Información de contacto y botón -->
+      <div class="flex items-center">
+        <div class="mr-4">
+          <a
+          class="flex items-center justify-center sm:justify-start gap-1.5 group"
+          href="/"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5 text-white shrink-0"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>
+          <span class="text-white transition">
+            presidencia@kinesiologosmza.com
+          </span>
+        </a>
+        </div>
+        <div class="mr-4">
+          <a
+          class="flex items-center justify-center sm:justify-start gap-1.5 group"
+          href="/"
+        >
+        <svg width="17" height="17" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M16.2037 12.2681L13.6637 11.9781C13.365 11.943 13.0623 11.9761 12.7782 12.0748C12.4942 12.1735 12.2362 12.3353 12.0237 12.5481L10.1837 14.3881C7.34486 12.9443 5.03745 10.6369 3.59367 7.79807L5.44367 5.94807C5.87367 5.51807 6.08367 4.91807 6.01367 4.30807L5.72367 1.78807C5.66698 1.30024 5.43287 0.850304 5.06591 0.523915C4.69895 0.197526 4.22478 0.0174842 3.73367 0.0180678H2.00367C0.873674 0.0180678 -0.0663265 0.958068 0.00367348 2.08807C0.533673 10.6281 7.36367 17.4481 15.8937 17.9781C17.0237 18.0481 17.9637 17.1081 17.9637 15.9781V14.2481C17.9737 13.2381 17.2137 12.3881 16.2037 12.2681Z" fill="white"/>
+          </svg>
+        <span class="text-white">123-456-7890</span>
+      </a>
+          
+        </div>
+        <button class="bg-blue-700 text-white px-3 py-[0.5px] rounded">
+<span>Login</span>            
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Menú de navegación -->
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex justify-end">
+      <div class="flex space-x-4 border-white border-t-[0.5px]">
+        <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Inicio</a>
+        <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Quiénes somos</a>
+        <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Kine Club</a>
+        <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Noticias</a>
+        <a href="#" class="text-white px-3 py-2 rounded-md text-sm font-medium">Obras sociales y prepagas</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
