1f5f930a5368f5d817593b94c69d19ca8fdf5852
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-05-17 09:18:36 -0300

    Carrousel para empresas

--- anothertestapp/resources/views/home/emp.blade.php
+++ anothertestapp/resources/views/home/emp.blade.php
@@ -1,12 +1,32 @@
-{{-- corrido de empresas --}}
-
-<div class="lg:my-6 md:my-10 flex flex-row justify-around">
-   
-  <img src="{{asset('storage/clients/logo-omint.svg')}}" class="w-[10%]" loading="lazy" alt="client logo" width="" height="">
-  <img src="{{asset('storage/clients/logo-avalian.svg')}}" class="w-[10%]" loading="lazy" alt="client logo" width="" height="">
-  <img src="{{asset('storage/clients/logo-swiss.svg')}}" class="w-[15%]" loading="lazy" alt="client logo" width="" height="">
-  <img src="{{asset('storage/clients/logo-dasuten.svg')}}" class="w-[10%]" loading="lazy" alt="client logo" width="" height="">
-  <img src="{{asset('storage/clients/logo-medicus.svg')}}" class="w-[10%]" loading="lazy" alt="client logo" width="" height="">
-  <img src="{{asset('storage/clients/logo-federada.svg')}}" class="w-[10%]" loading="lazy" alt="client logo" width="" height="">
-    
+<div class="swiper my-6 md:my-10">
+  <div class="swiper-wrapper flex">
+    <div class="swiper-slide lg:my-6 md:my-10 flex flex-row justify-around">
+      <img src="{{asset('storage/clients/logo-omint.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-avalian.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-swiss.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-dasuten.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-medicus.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+    </div>
+    <div class="swiper-slide lg:my-6 md:my-10 flex flex-row justify-around">
+      <img src="{{asset('storage/clients/logo-swiss.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-dasuten.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-medicus.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-federada.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-avalian.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+    </div>
+    <div class="swiper-slide lg:my-6 md:my-10 flex flex-row justify-around">
+      <img src="{{asset('storage/clients/logo-swiss.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-dasuten.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-medicus.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-federada.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-avalian.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+    </div>
+    <div class="swiper-slide lg:my-6 md:my-10 flex flex-row justify-around">
+      <img src="{{asset('storage/clients/logo-swiss.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-dasuten.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-medicus.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-federada.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+      <img src="{{asset('storage/clients/logo-avalian.svg')}}" alt="client logo" class=" sm:w-1/5 " width="100px">
+    </div>
+  </div>
 </div>
--- anothertestapp/resources/views/layout.blade.php
+++ anothertestapp/resources/views/layout.blade.php
@@ -5,12 +5,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', 'Circulo de Kinesiologos Mendoza')</title>
   <link rel="icon" type="image/png" sizes="16x16" href="{{asset('storage/favicon/favicon.png')}}" /> 
-  {{-- <link rel="icon" type="image/png" sizes="16x16" href="../resources/img/favicon/favicon-16x16.png" /> --}}
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
   @vite('resources/css/app.css')
 </head>
 <body > 
   @yield('content')
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script> 
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+  <script>
+    var swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 9,
+  autoplay: {
+    delay: 2000, 
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+  </script>
 </body>
 </html>
