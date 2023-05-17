8b419a336acdd2ecb5bf47278e0b6149e01c4fb7
dmampel <105382023+dmampel@users.noreply.github.com>
2023-05-17 01:33:08 -0300

    Responsive quienes blade

--- anothertestapp/resources/views/home/quienes.blade.php
+++ anothertestapp/resources/views/home/quienes.blade.php
@@ -1,7 +1,7 @@
 {{-- Quienes somos --}}
-<div class="space-y-6 justify-between text-gray-600 md:flex flex-row-reverse md:gap-6 md:space-y-0 lg:gap-12 lg:items-center">
+<div class="space-y-6 justify-between text-gray-600 lg:flex flex-row-reverse lg:gap-6 lg:space-y-0 xl:gap-12 lg:items-center sm:grid sm:grid-rows-1 sm:mt-8 lg:mt-0">
 
-    <div class=" md:7/12 lg:w-1/2">
+    <div class=" lg:w-1/2 sm:h-fit">
       <h2 class="slide-in-bottom-h1 text-3xl text-center font-bold text-black md:text-4xl">
         Quienes somos
       </h2>
@@ -12,19 +12,20 @@
       </div>
       
       <div class="flex justify-center w-full">
-        <button class=" bounce-top-icons bg-blue-700 hover:bg-blue-500 text-white px-5  py-2 rounded-[10px] "><a href="../quienes/index.blade.php">Saber Más</a></button>
+        <button class=" invisible lg:visible bounce-top-icons bg-blue-700 hover:bg-blue-500 text-white px-5  py-2 rounded-[10px] "><a href="../quienes/index.blade.php">Saber Más</a></button>
       </div>
       
     </div>
     <div class="md:4/10 lg:w-1/2 flex justify-center">
-      <img class="start-0 absolute wobble-hor-bottom" src="{{asset ('storage/png/geometric/rectangle.svg')}}" alt="">
-      <div class="end-[50%] top-[150%] absolute wobble-ver-right">
-        <svg width="240" height="240" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <img class="start-0  wobble-hor-bottom lg:w-[40%] sm:w-[60%]" src="{{asset ('storage/png/geometric/rectangle.svg')}}" alt="">
+      <div class="end-[50%] lg:top-[150%] md:top-[190%] absolute wobble-ver-right z-[2] lg:right-96 2xl:right-[45rem] ">
+        <svg class="2xl:w-[240px] lg:w-3/5 sm:w-[55%]"width="240" height="240" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle  cx="120.001" cy="120" r="120" fill="#F28F1C"/>
           </svg>
       </div>
-      <img class="w-[60%] z-0" src="{{asset ('storage/png/imagen.png')}}" alt="image" loading="lazy" width="" height="" class="w-full">
+      <img class="w-[60%] z-0 lg:mr-0 sm:mr-24" src="{{asset ('storage/png/imagen.png')}}" alt="image" loading="lazy" width="" height="" class="w-full">
       
 
     </div>
+    <button class=" visible lg:invisible bg-blue-700 hover:bg-blue-500 text-white text-2xl py-8 mb-8 mt-0"><a href="../quienes/index.blade.php">Saber Más</a></button>
   </div>
