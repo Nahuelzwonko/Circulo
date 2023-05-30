addb38636da20c9eb20fd1db7ab7a746ffbe5a10
dmampel <105382023+dmampel@users.noreply.github.com>
2023-05-30 19:33:18 -0300

    avances en profesionales

--- anothertestapp/resources/views/profesionales/index.blade.php
+++ anothertestapp/resources/views/profesionales/index.blade.php
@@ -1,36 +1,40 @@
 @extends('master')
 @section('titulo', 'Profesionales asociados | CKFM')
 @section('Encabezado', 'Personal asociado')
-@section('cont', '')
+
+@section('cont', 'Aquí encontraras a cada uno de los profesionales asociados. Te invitamos a utilizar los filtros para agilizar tu búsqueda.')
+@section('Boton', 'Infórmate')
 @section('content')
     <section class="h-[100vh] flex flex-col items-center mt-20">
         <h1 class="font-hammersmith text-7xl">Búsqueda</h1>
         <div class="w-[80%] relative overflow-x-auto  sm:rounded-lg">
-            <div class="flex items-center mb-4">
-                <input id="default-checkbox" type="checkbox" value=""
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Atencion
-                    Domi</label>
-            </div>
-            {{--  --}}
-            <div class="flex items-center justify-between py-4 bg-white dark:bg-gray-800">
-                <div>
+            <div class="flex flex-col lg:flex-row-reverse justify-evenly items-center p-8">
+                <div class="flex items-center mb-4">
+                    <input id="default-checkbox" type="checkbox" value=""
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="default-checkbox" class="ml-2 text-lg font-medium text-gray-900 dark:text-gray-300 ">Atención Domiciliaria</label>
                 </div>
-                <label for="table-search" class="sr-only">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                clip-rule="evenodd"></path>
-                        </svg>
+                {{--  --}}
+                <div class="flex items-center justify-between py-4 bg-white dark:bg-gray-800">
+                    <div>
                     </div>
-                    <input type="text" id="table-search-users"
-                        class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Buscar por nombre o localidad">
+                    <label for="table-search" class="sr-only"></label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
+                        <input type="text" id="table-search-users"
+                            class="block p-2 pl-10 text-lg text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Buscar por nombre o localidad">
+                    </div>
                 </div>
             </div>
+          
             <table class="w-full text-left text-gray-500 dark:text-gray-400 text-lg">
                 <thead
                     class="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-[1px] border-slate-300">
@@ -42,10 +46,10 @@
                             <i class="fa-solid fa-sharp fa-location-dot mr-2"></i>Dirección Consultorio
                         </th>
                         <th scope="col" class="px-6 py-4">
-                            <i class="fa-solid fa-tag fa-flip-horizontal mr-2"></i>Especialidad
+                            <i class="fa-solid fa-globe mr-2"></i>Localidad
                         </th>
                         <th scope="col" class="px-6 py-4">
-                            <i class="fa-solid fa-tag fa-flip-horizontal mr-2"></i>Localidad
+                            <i class="fa-solid fa-tag fa-flip-horizontal mr-2"></i>Especialidad
                         </th>
                         <th scope="col" class="px-6 py-4">
                             <i class="fa-solid fa-phone mr-2"></i>Contacto
@@ -62,12 +66,13 @@
                             <td class="px-6 py-4 uppercase">
                                 <p>{{ $person->domicilio }}</p>
                             </td>
-                            <td class="px-6 py-4 uppercase">
-                                <p>{{ $person->especialidad }}</p>
-                            </td>
                             <td>
                                 <p>{{ $person->localidad }}</p>
                             </td>
+                            <td class="px-6 py-4 uppercase">
+                                <p>{{ $person->especialidad }}</p>
+                            </td>
+                        
                             <td class="px-6 py-4 uppercase">
                                 <p>{{ $person->telefono }}</p>
                             </td>
