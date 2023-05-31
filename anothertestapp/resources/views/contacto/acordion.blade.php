b385400f337fe68799b59f0aef4c58af0d93134f
dmampel <105382023+dmampel@users.noreply.github.com>
2023-05-31 17:05:43 -0300

    Update acordion.blade.php

--- anothertestapp/resources/views/contacto/acordion.blade.php
+++ anothertestapp/resources/views/contacto/acordion.blade.php
@@ -1,6 +1,6 @@
 
 <div class="px-8 lg:px-[20%] bg-black flex flex-col pb-16">
-  <h1 class="font-hammersmith text-xl lg:text-5xl text-center capitalize text-white mt-8 py-8">Contestamos tus preguntas más frequentes!</h1>
+  <h1 class="font-hammersmith text-2xl lg:text-5xl text-center capitalize text-white mt-8 py-8">Contestamos tus preguntas más frequentes!</h1>
   <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-black dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
     <h2 id="accordion-flush-heading-1">
       <button type="button" class="capitalize flex items-center justify-between w-full py-5 font-medium text-left text-white border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
@@ -29,7 +29,7 @@
     </div>
     <h2 id="accordion-flush-heading-3">
       <button type="button" class="capitalize flex items-center justify-between w-full py-5 font-medium text-left text-white border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
-        <span class="text-lg lg:text-2xl font-Poppins">¿Hay algun cargo adicional?</span>
+        <span class="text-lg lg:text-2xl font-Poppins">¿Cómo puedo contactarme?</span>
         <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
     </h2>
