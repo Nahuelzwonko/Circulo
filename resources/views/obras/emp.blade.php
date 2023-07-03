77fb972262a9dfe643604baf2cad65e7c56043be
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-03 20:42:40 -0300

    cambios

--- resources/views/obras/emp.blade.php
+++ resources/views/obras/emp.blade.php
@@ -1,4 +1,4 @@
-    <section class="w-full h-full flex justify-center items-center mt-40">
+    <section class="w-full h-full flex justify-center items-center mt-20">
         <div class="flex flex-wrap justify-center w-[70%] ">
             @foreach ($datos as $dato)
                 <div class="relative group w-full md:w-1/1 lg:w-1/2 xl:w-1/3 p-6">
@@ -16,7 +16,7 @@
                             </button>
                         </div>
                         <!-- Main modal -->
-                        <div id="defaultModal{{ $dato->id }}"  tabindex="-1" aria-hidden="true"
+                        <div id="defaultModal{{ $dato->id }}" tabindex="-1" aria-hidden="true"
                             class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                             <div class="relative w-full max-w-2xl max-h-full">
                                 <!-- Modal content -->
@@ -30,8 +30,8 @@
                                         <button type="button"
                                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                                             data-modal-hide="defaultModal{{ $dato->id }}">
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
+                                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd"
                                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                                     clip-rule="evenodd"></path>
