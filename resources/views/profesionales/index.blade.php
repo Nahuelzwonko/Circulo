d6397a69d92b0bda37e15c04aeb003db228e2cf4
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-04 05:20:35 -0300

    cambios

--- resources/views/profesionales/index.blade.php
+++ resources/views/profesionales/index.blade.php
@@ -89,9 +89,9 @@
         <div class="flex flex-col items-center">
             <!-- Help text -->
             <span class="text-sm text-gray-700 dark:text-gray-400">
-                Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span
-                    class="font-semibold text-gray-900 dark:text-white">10</span> of <span
-                    class="font-semibold text-gray-900 dark:text-white">100</span> Entries
+                Mostrando <span class="font-semibold text-gray-900 dark:text-white">1</span> a <span
+                    class="font-semibold text-gray-900 dark:text-white">10</span> de <span
+                    class="font-semibold text-gray-900 dark:text-white">100</span> Entradas
             </span>
             <div class="inline-flex mt-2 xs:mt-0">
                 <!-- Buttons -->
@@ -104,11 +104,11 @@
                             d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    Prev
+                    Anterior
                 </button>
                 <button id="next-button"
                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Next
+                    Próximo
                     <svg aria-hidden="true" class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
