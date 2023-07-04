e46b4e6c967c6cd9c45b0b2072131072849adc8f
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-04 05:16:45 -0300

    cambios

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,6 +7,8 @@
     <title>@yield('titulo', 'Circulo de Kinesiologos Mendoza')</title>
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/favicon/favicon.png') }}" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
+
     @vite('resources/css/app.css')
     <script src="https://kit.fontawesome.com/35eba4cb11.js" crossorigin="anonymous"></script>
     <style>
