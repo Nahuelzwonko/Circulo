886795a4e1cb02383d6ff20e921d7b29f8037dc8
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-04 06:33:54 -0300

    cambios

--- resources/views/obras/emp.blade.php
+++ resources/views/obras/emp.blade.php
@@ -2,8 +2,8 @@
         <div class="flex flex-wrap justify-center w-[70%] ">
             @foreach ($datos as $dato)
                 <div class="relative group w-full md:w-1/1 lg:w-1/2 xl:w-1/3 p-6">
-                    <div class="w-[200px] h-[200px] border rounded-xl shadow-xl flex justify-center items-center">
-                        <img class="w-[200px] h-[200px] object-cover rounded-lg" src="{{ asset($dato->imagen) }}"
+                    <div class="w-[200px] h-[200px] border rounded-xl shadow-xl flex justify-center items-center bg-white">
+                        <img class="w-full h-cover object-cover rounded-lg" src="{{ asset($dato->imagen) }}"
                             alt="{{ $dato->nombre }}" />
                         <div
                             class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-filter backdrop-blur">
