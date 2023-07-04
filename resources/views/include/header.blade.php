69eb7bf1125fc0b9cd9eba6ff865211311aa0810
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-04 01:12:14 -0300

    cambios

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -7,14 +7,10 @@
 </style>
 
 <div class="main-container md:h-[750px] sm:h-auto bg-hero-bg">
-    <!-- Incluyo navbar -->
     @include('include.nav')
-    <!-- Padre grid para dividir en dos columnas el contenido: queda el texto a la derecha y la imagen a la izquierda. -->
     <div class="grid lg:grid-cols-2 sm:grid-cols-1 lg:mt-10 mt-5 mb:mb-8 sm:mb-0 md:gap-8 lg:gap-0">
-        <!-- Hijo 1, ordena el contenido -->
         <div
-            class="flex flex-col lg:items-center sm:items-center lg:w-3/4 sm:w-full sm:h-28 gap-8 2xl:ml-64 lg:ml-48 mt-15">
-            <!-- Título, subtitulo, botón. -->
+            class="flex flex-col lg:items-center sm:items-center lg:w-3/4 sm:w-full sm:h-28 gap-8 2xl:ml-64 lg:ml-48 mt-15 text-center">
             <h1
                 class="font-hammersmith 2xl:text-7xl xl:text-6xl sm:text-4xl font-bold uppercase text-white 2xl:leading-[6rem] xl:leading-[4.5rem] lg:leading-normal sm:leading-[3.5rem] lg:text-start sm:text-center">
                 El círculo que <br>nos une</br></h1>
@@ -22,9 +18,7 @@
             <button
                 class="xl:text-xl text-white p-4 rounded-full bg-[#0051A7] hover:bg-blue-700 self-center invisible lg:visible">Conocénos!</button>
         </div>
-        <!-- Hijo 2, contiene la imagen de la derecha. -->
         <div>
-            <!-- Imagen -->
             <img class="h-[90%] lg:w-auto md:w-[33.7%] m-auto lg:mt-0 md:mt-4 sm:w-[250px] sm:mt-4 rounded-tl-[160px] rounded-tr-3xl rounded-br-[160px] rounded-bl-3xl "src="{{ asset('storage/img-personas/hero-img.svg') }}"
                 alt="">
         </div>
