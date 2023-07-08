d3b2ed226bb4879620d4423c482483e4d35bfe96
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-08 09:19:03 -0300

    cambios

--- resources/views/allPost.blade.php
+++ resources/views/allPost.blade.php
@@ -2,6 +2,7 @@
 @section('titulo', 'Noticias | CKFM')
 @section('contenido')
 @section('Encabezado', 'Nuestro blog')
+@section('cont', 'Explora nuestro blog de noticias para mantenerte actualizado sobre avances, descubrimientos y novedades en kinesiología y fisioterapia. Te brindamos información relevante y consejos prácticos para cuidar tu salud y bienestar de forma óptima. Únete a la conversación, comparte tus comentarios y juntos promovamos una vida activa y saludable. Aprendamos y crezcamos juntos.')
 @section('content')
 @section('Boton', 'Informate!')
 <div class="mt-5 lg:mt-20">
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,10 +2,10 @@
 @section('title', 'Vista de la noticia | CKFM')
 @section('content')
 @section('Encabezado', 'Nuestro blog')
+@section('cont', 'Explora nuestro blog de noticias para mantenerte actualizado sobre avances, descubrimientos y novedades en kinesiología y fisioterapia. Te brindamos información relevante y consejos prácticos para cuidar tu salud y bienestar de forma óptima. Únete a la conversación, comparte tus comentarios y juntos promovamos una vida activa y saludable. Aprendamos y crezcamos juntos.')
 @section('Boton', 'Informate!')
 @section('content')
 @include('posts.view')
-
 @include('include.cta')
 @include('include.footer')  
 
