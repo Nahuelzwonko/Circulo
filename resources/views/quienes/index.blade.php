92cbe692dba9fef548d1bfd07f40fca77dcc91ab
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-08 08:59:26 -0300

    cambios

--- resources/views/quienes/index.blade.php
+++ resources/views/quienes/index.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 @section('titulo', 'Quienes somos | CKFM')
 @section('Encabezado', 'Quienes somos')
-@section('cont', 'aca va el encabezado ')
+@section('cont', 'En el Círculo de Kinesiólogos y Fisioterapeutas de Mendoza, nos unimos para fortalecer la profesión y proteger los intereses de nuestros socios. Negociamos convenios, promovemos la formación continua y difundimos los beneficios de la kinesiología y la fisioterapia. Únete a nuestra comunidad comprometida con la excelencia y el crecimiento en la salud y el bienestar.')
 @section('Boton', 'Conocénos')
 @section('content')
 <div class="mt-5 lg:mt-20">
