19d3deb79bac7bef94f206f7f2cbef462bd84cd8
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-12 12:54:12 -0300

    usuarios

--- resources/views/layouts/temlogin.blade.php
+++ resources/views/layouts/temlogin.blade.php
@@ -15,7 +15,7 @@
     @yield('content')
     <div class="min-h-screen bg-blue-500 flex justify-center items-center">
         <div
-            class="absolute w-60 h-60 rounded-xl bg-orange-400 -top-5 -left-16 z-0 transform rotate-45 hidden md:block">
+            class="absolute w-60 h-60 rounded-xl bg-[#F28F1C] -top-5 -left-16 z-0 transform rotate-45 hidden md:block">
         </div>
         <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
             <div>
@@ -23,9 +23,9 @@
             </div>
             @yield('form')
         </div>
-        <div class="w-40 h-40 absolute bg-orange-400 rounded-full top-0 right-12 hidden md:block"></div>
+        <div class="w-40 h-40 absolute bg-[#F28F1C] rounded-full top-0 right-12 hidden md:block"></div>
         <div
-            class="w-20 h-40 absolute bg-orange-400 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block">
+            class="w-20 h-40 absolute bg-[#F28F1C] rounded-full bottom-20 left-10 transform rotate-45 hidden md:block">
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
