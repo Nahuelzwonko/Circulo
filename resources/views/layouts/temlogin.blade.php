e475627a992d81107d151e88c58454955086f57a
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-14 09:56:12 -0300

    usuarios

--- resources/views/layouts/temlogin.blade.php
+++ resources/views/layouts/temlogin.blade.php
@@ -10,32 +10,41 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
     @vite('resources/css/app.css')
     <style>
-        .bg-naranja{
+        .bg-naranja {
             background-color: #F28F1C;
+            +background-repeat: none;
         }
     </style>
 </head>
 
-    <body>
-        @yield('content')
-        <div class="min-h-screen bg-blue-500 flex justify-center items-center">
-            <div
-                class="absolute w-60 h-60 rounded-xl bg-naranja -top-5 -left-16 z-0 transform rotate-45 hidden md:block">
-            </div>
-            <div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
-                <div>
-                    <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Ingresa a tu cuenta</h1>
+<body>
+    @yield('content')
+
+    <body class=" bg-[#546EF3]">
+        <!-- Container -->
+        <div class="container mx-auto">
+            <div class="flex justify-center px-6 my-12">
+                <!-- Row -->
+                <div class="w-full xl:w-3/4 lg:w-11/12 flex mt-20">
+                    <!-- Col -->
+                    <div class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                        style="background-image: url({{ asset('storage/card/login.svg') }}); background-position:center; ">
+                    </div>
+                    <!-- Col -->
+                    <div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+                        <h3 class="pt-4 text-2xl text-center">Sign up</h3>
+                        <div class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                            @yield('form')
+                        </div>
+                    </div>
                 </div>
-                @yield('form')
-            </div>
-            <div class="w-40 h-40 absolute bg-naranja rounded-full top-0 right-12 hidden md:block"></div>
-            <div
-                class="w-20 h-40 absolute bg-naranja rounded-full bottom-20 left-10 transform rotate-45 hidden md:block">
             </div>
         </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
     </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+</body>
 
 
 
 </html>
+<!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
