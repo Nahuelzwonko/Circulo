b98d7188da53f66c219c038f2adc5b6fc1435c55
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-19 20:49:39 -0300

    usuarios

--- resources/views/user_home.blade.php
+++ resources/views/user_home.blade.php
@@ -36,7 +36,7 @@
                     <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
                 </div>
             </div>
-            <div class="flex sm:flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+            <div class="flex flex-wrap sm:flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
                 <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                     <div
                         class=" inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
