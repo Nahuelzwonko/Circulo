8c90aebe285460966dde701ccc9fb8f5248e45fb
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-19 20:45:56 -0300

    usuarios

--- resources/views/users/include/nav.blade.php
+++ resources/views/users/include/nav.blade.php
@@ -17,7 +17,7 @@
     class="fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white dark:bg-gray-800"
     tabindex="-1" aria-labelledby="drawer-navigation-label">
     <div class="w-full flex justify-center">
-        <img class="w-[50%]" src="{{ asset('storage/logo/logoblack.svg') }}" alt="">
+        <img class="w-1/2 " src="{{ asset('storage/logo/logoblack.svg') }}" alt="">
     </div>
     <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation"
         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
