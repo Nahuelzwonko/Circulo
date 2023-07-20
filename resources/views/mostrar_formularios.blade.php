6ae7acf80d68eaea15f63ac6bef8ff6b912c0201
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-19 21:36:53 -0300

    usuarios

--- resources/views/mostrar_formularios.blade.php
+++ resources/views/mostrar_formularios.blade.php
@@ -21,42 +21,45 @@
                         <tr>
                             <th scope="col" class="px-6 py-3">Nombre del Profesional</th>
                             <th scope="col" class="px-6 py-3">Nombre del Paciente</th>
-                            <th scope="col" class="px-6 py-3">Accion</th>
-                            <th scope="col" class="px-6 py-3" >Descargar</th>
-
+                            <th scope="col" class="px-6 py-3">Editar</th>
+                            <th scope="col" class="px-6 py-3">Eliminar</th>
+                            <th scope="col" class="px-6 py-3">Descargar</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="">
                         @foreach ($formularios as $formulario)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <td class="px-6 py-4">{{ $formulario->nombre_profesional }}</td>
                                 <td class="px-6 py-4">{{ $formulario->nombre_paciente }}</td>
                                 <td class="px-6 py-4">
-                                    <div class="w-full flex flex-col justify-center  align-top">
-                                        <div class="flex  justify-center">
-                                            <a href="{{ route('formulario.edit', $formulario->id) }}"
-                                                class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
-                                        </div>
+                                    <div class="flex justify-center items-center">
+                                        <a href="{{ route('formulario.edit', $formulario->id) }}"
+                                            class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
+                                    </div>
 
 
-
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="flex justify-center items-center">
                                         <form action="{{ route('formulario.destroy', $formulario->id) }}" method="post">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit"
                                                 onclick="return confirm('¿Estás seguro de que deseas eliminar este formulario?')"
-                                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Eliminar
+                                                class=" text-white bg-red-700 hover:bg-red-800 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  ">Eliminar
                                             </button>
                                         </form>
                                     </div>
-
                                 </td>
                                 <td>
-                                    @foreach ($formularios as $formulario)
-                                        <!-- Mostrar la información del formulario aquí -->
-                                        <a href="{{ route('formulario.pdf', $formulario->id) }}"
-                                            class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Descargar PDF</a>
-                                    @endforeach
+                                    <div class="flex justify-center items-center">
+                                        @foreach ($formularios as $formulario)
+                                            <!-- Mostrar la información del formulario aquí -->
+                                            <a href="{{ route('formulario.pdf', $formulario->id) }}"
+                                                class=" text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Descargar
+                                                PDF</a>
+                                        @endforeach
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
--- resources/views/user_home.blade.php
+++ resources/views/user_home.blade.php
@@ -3,6 +3,7 @@
 @include('users.include.nav')
 
 @section('content')
+
     <section class="text-gray-600 body-font ">
         <div class="w-full flex justify-center mt-5">
             <img src="{{ asset('storage/logo/logoblack.svg') }}" alt="">
@@ -37,6 +38,7 @@
                 </div>
             </div>
             <div class="flex flex-wrap sm:flex-row sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+              
                 <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                     <div
                         class=" inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
