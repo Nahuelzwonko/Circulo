c78bc9ad1e26e99b939b9723c54c8f9c5a6dc337
dmampel <105382023+dmampel@users.noreply.github.com>
2023-07-20 04:00:31 -0300

    Update ficha_kinesica.blade.php

--- resources/views/pdf/ficha_kinesica.blade.php
+++ resources/views/pdf/ficha_kinesica.blade.php
@@ -54,18 +54,39 @@
     .field-value {
         flex: 2;
     }
+    .firmas{
+        display:flex;
+        flex-direction:row;
+    }
 </style>
 
 <body class="bg-gray-100">
     <h1>Ficha Médica</h1>
-
+    <p><strong>fecha</strong>{{$formulario->created_at}}</p>
     <p><strong>Nombre del profesional:</strong> {{ $formulario->nombre_profesional }}</p>
     <p><strong>Nombre del paciente:</strong> {{ $formulario->nombre_paciente }}</p>
     <p><strong>Edad:</strong> {{ $formulario->edad }}</p>
     <p><strong>Obra social:</strong> {{ $formulario->obra_social }}</p>
+    <p><strong>Nro Carnet:</strong></p>
+    <p><strong>Domicilio:</strong> {{$formulario->domicilio}}</p><p><strong></strong> {{$formulario->localidad}}</p>
+    <p><strong>Teléfono particular:</strong> {{$formulario->telefono_particular}}</p>
+    <p><strong>Teléfono celular:</strong> {{$formulario->telefono_celular}}</p>
+    <p><strong>Fecha de Nacimiento:</strong> {{$formulario->fecha_nacimiento}} , </p><p><strong>Edad</strong> {{$formulario->edad}}</p>
+    <p><strong>DNI:</strong> {{$formulario->dni}}</p>
+    <p><strong>Profesional Derivante:</strong> {{$formulario->profesional_derivantre}}</p><p><strong>Matricula:</strong> {{$formulario->mat}}</p>
+    <p><strong>Profesión:</strong> {{$formulario->profesion_ocupacion}}</p>
 
+    <p><strong>Antecedentes Familiares:</strong> {{$formulario->antecedentes_familiares}}</p>
+    <p><strong>Diagnóstico de Ingreso:</strong> {{$formulario->diagnostico_ingreso}}</p>
+    <p><strong>Evaluación Kinesica</strong></p>
+    <p><strong>Plan de Tratamiento:</strong> {{$formulario->plan_tratamiento}}</p>
+    <p><strong>Objetivos del Tratamiento:</strong> {{$formulario->objetivos_tratamiento}}</p>
+    <div class="firmas">
+        <p><strong>Firma del paciente:</strong> {{$formulario->fecha_nacimiento}}</p>
+        <p><strong>Fecha del profesional :</strong> {{$formulario->fecha_nacimiento}}</p>
 
-
+    </div>
+    
 </body>
 
 </html>
