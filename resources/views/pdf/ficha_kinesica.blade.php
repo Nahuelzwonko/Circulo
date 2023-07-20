c6d6ad09f1e2bce83e7b54d8c7cbcd793c23b841
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-20 14:43:25 -0300

    usuarios

--- resources/views/pdf/ficha_kinesica.blade.php
+++ resources/views/pdf/ficha_kinesica.blade.php
@@ -14,79 +14,130 @@
         padding: 20px;
     }
 
-    p {
-        font-size: 14px;
-    }
-
-    h1 {
-        font-size: 24px;
-        font-weight: bold;
-        text-align: center;
-        margin-bottom: 20px;
-    }
-
-    .section {
-        margin-bottom: 30px;
-    }
-
-    .section-title {
-        font-size: 18px;
-        font-weight: bold;
-        margin-bottom: 10px;
-    }
-
-    .section-content {
-        border: 1px solid #ccc;
-        padding: 20px;
-        background-color: #f9f9f9;
-    }
-
-    .field-group {
+    .titulo {
+        width: 100%;
+        height: auto;
         display: flex;
-        margin-bottom: 15px;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid black;
+        text-align: center;
     }
 
-    .field-label {
-        flex: 1;
-        font-weight: bold;
+    .titulo_h {
+        font-size: 20px;
     }
 
-    .field-value {
-        flex: 2;
+    .parent {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: 1fr;
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
     }
-    .firmas{
-        display:flex;
-        flex-direction:row;
+
+    .div1 {
+        grid-area: 1 / 1 / 2 / 2;
+    }
+
+    .div2 {
+        grid-area: 1 / 2 / 2 / 3;
+    }
+
+    .flex-container {
+        display: inline-flex;
+        flex-direction: row;
+        flex-wrap: wrap-reverse;
+        justify-content: flex-start;
+        align-items: center;
+        align-content: normal;
+    }
+
+    .flex-items:nth-child(1) {
+        border: 1px solid black;
+        display: block;
+        flex-grow: 0;
+        flex-shrink: 1;
+        flex-basis: auto;
+        align-self: auto;
+        order: 0;
+    }
+
+    .flex-items:nth-child(2) {
+        border: 1px solid black;
+        display: block;
+        flex-grow: 0;
+        flex-shrink: 1;
+        flex-basis: auto;
+        align-self: auto;
+        order: 0;
+    }
+
+    .flex-items:nth-child(3) {
+        border: 1px solid black;
+        display: block;
+        flex-grow: 0;
+        flex-shrink: 1;
+        flex-basis: auto;
+        align-self: auto;
+        order: 0;
+    }
+
+    .flex-items:nth-child(4) {
+        border: 1px solid black;
+        display: block;
+        flex-grow: 0;
+        flex-shrink: 1;
+        flex-basis: auto;
+        align-self: auto;
+        order: 0;
     }
 </style>
 
 <body class="bg-gray-100">
-    <h1>Ficha Médica</h1>
-    <p><strong>Fecha y Hora de Ingreso: </strong>{{$formulario->created_at}}</p>
+    <div class="titulo">
+        <h1 class="titulo_h">Historia clinica</h1>
+    </div>
+    <div class="parent">
+        <div class="div1"> {{ $formulario->created_at }} </div>
+        <div class="div2">
+            <div class="flex-container">
+                <div class="flex-items"></div>
+                <div class="flex-items"></div>
+                <div class="flex-items"></div>
+                <div class="flex-items"></div>
+            </div>
+
+        </div>
+    </div>
+
     <p><strong>Nombre del profesional:</strong> {{ $formulario->nombre_profesional }}</p>
     <p><strong>Nombre del paciente:</strong> {{ $formulario->nombre_paciente }}</p>
     <p><strong>Edad:</strong> {{ $formulario->edad }}</p>
     <p><strong>Obra social:</strong> {{ $formulario->obra_social }}</p>
     <p><strong>Nro Carnet:</strong></p>
-    <p><strong>Domicilio:</strong> {{$formulario->domicilio}}</p><p><strong></strong> {{$formulario->localidad}}</p>
-    <p><strong>Teléfono particular:</strong> {{$formulario->telefono_particular}}</p>
-    <p><strong>Teléfono celular:</strong> {{$formulario->telefono_celular}}</p>
-    <p><strong>Fecha de Nacimiento:</strong> {{$formulario->fecha_nacimiento}} , </p><p><strong>Edad</strong> {{$formulario->edad}}</p>
-    <p><strong>DNI:</strong> {{$formulario->dni}}</p>
-    <p><strong>Profesional Derivante:</strong> {{$formulario->profesional_derivantre}}</p><p><strong>Matricula:</strong> {{$formulario->mat}}</p>
-    <p><strong>Profesión:</strong> {{$formulario->profesion_ocupacion}}</p>
+    <p><strong>Domicilio:</strong> {{ $formulario->domicilio }}</p>
+    <p><strong></strong> {{ $formulario->localidad }}</p>
+    <p><strong>Teléfono particular:</strong> {{ $formulario->telefono_particular }}</p>
+    <p><strong>Teléfono celular:</strong> {{ $formulario->telefono_celular }}</p>
+    <p><strong>Fecha de Nacimiento:</strong> {{ $formulario->fecha_nacimiento }} , </p>
+    <p><strong>Edad</strong> {{ $formulario->edad }}</p>
+    <p><strong>DNI:</strong> {{ $formulario->dni }}</p>
+    <p><strong>Profesional Derivante:</strong> {{ $formulario->profesional_derivantre }}</p>
+    <p><strong>Matricula:</strong> {{ $formulario->mat }}</p>
+    <p><strong>Profesión:</strong> {{ $formulario->profesion_ocupacion }}</p>
 
-    <p><strong>Antecedentes Familiares:</strong> {{$formulario->antecedentes_familiares}}</p>
-    <p><strong>Diagnóstico de Ingreso:</strong> {{$formulario->diagnostico_ingreso}}</p>
+    <p><strong>Antecedentes Familiares:</strong> {{ $formulario->antecedentes_familiares }}</p>
+    <p><strong>Diagnóstico de Ingreso:</strong> {{ $formulario->diagnostico_ingreso }}</p>
     <p><strong>Evaluación Kinesica</strong></p>
-    <p><strong>Plan de Tratamiento:</strong> {{$formulario->plan_tratamiento}}</p>
-    <p><strong>Objetivos del Tratamiento:</strong> {{$formulario->objetivos_tratamiento}}</p>
+    <p><strong>Plan de Tratamiento:</strong> {{ $formulario->plan_tratamiento }}</p>
+    <p><strong>Objetivos del Tratamiento:</strong> {{ $formulario->objetivos_tratamiento }}</p>
     <div class="firmas">
-        <p><strong>Firma del paciente:</strong> {{$formulario->fecha_nacimiento}}</p>
-        <p><strong>Fecha del profesional :</strong> {{$formulario->fecha_nacimiento}}</p>
+        <p><strong>Firma del paciente:</strong> {{ $formulario->fecha_nacimiento }}</p>
+        <p><strong>Fecha del profesional :</strong> {{ $formulario->fecha_nacimiento }}</p>
 
     </div>
-    
+
 </body>
 
 </html>
