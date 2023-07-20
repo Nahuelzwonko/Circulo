ad3629f986030a78f692a09ef9fe47515035c502
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-07-19 21:40:33 -0300

    usuarios

--- resources/views/mostrar_formularios.blade.php
+++ resources/views/mostrar_formularios.blade.php
@@ -46,7 +46,7 @@
                                             @method('DELETE')
                                             <button type="submit"
                                                 onclick="return confirm('¿Estás seguro de que deseas eliminar este formulario?')"
-                                                class=" text-white bg-red-700   font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  ">Eliminar
+                                                class="text-white  bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Eliminar
                                             </button>
                                         </form>
                                     </div>
