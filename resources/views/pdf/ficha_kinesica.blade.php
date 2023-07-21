fffad42171e1f537f39297fc01e8ed8e6bbc4615
dmampel <105382023+dmampel@users.noreply.github.com>
2023-07-21 00:57:33 -0300

    Update ficha_kinesica.blade.php

--- resources/views/pdf/ficha_kinesica.blade.php
+++ resources/views/pdf/ficha_kinesica.blade.php
@@ -1,143 +1,227 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19">
-</head>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        line-height: 1.5;
-        margin: 0;
-        padding: 20px;
-    }
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19">
+    </head>
+    <style>
+        
+        body{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 
-    .titulo {
-        width: 100%;
-        height: auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border: 1px solid black;
-        text-align: center;
-    }
+        }
+        .padre{
+            
+            
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            
+            
+        }
+        .fecha{
+        
+            
+            width: 100%;
+            text-align: left;
+            
+        }
+        .fecha_contenido{
+            margin: 0;
+        }
+        .profesional, .paciente{
+            text-align: left;
+        }
+        .titulo{
+            font-size: large;
+            font-weight: bold;
+            margin: 1rem 0;
+        }
+        .flex-row{
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            align-items: baseline;
+            justify-content: space-between;
+            
+        }
+        .flex-col{
+            display: flex;
+            flex-direction: column;
+            gap: 1.5rem;
+            
+            
+            
+        }
 
-    .titulo_h {
-        font-size: 20px;
-    }
+        .text-left{
+            text-align: left;
+        }
+        .paciente{
+            display: flex;
+            flex-direction: column;
+            
+            
+        }
 
-    .parent {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: 1fr;
-        grid-column-gap: 0px;
-        grid-row-gap: 0px;
-    }
+        .separador{
+            width: 100%;
+            height: 2px;
+            background-color: gainsboro;
+            border-radius: 100%;
+            margin-left: 10px;
+            
+        }
 
-    .div1 {
-        grid-area: 1 / 1 / 2 / 2;
-    }
+        .contenido{
+            margin-left: 2rem;
+            margin: 1rem 0;
+            font-size: medium;
+            text-transform:uppercase;
+        }
 
-    .div2 {
-        grid-area: 1 / 2 / 2 / 3;
-    }
 
-    .flex-container {
-        display: inline-flex;
-        flex-direction: row;
-        flex-wrap: wrap-reverse;
-        justify-content: flex-start;
-        align-items: center;
-        align-content: normal;
-    }
 
-    .flex-items:nth-child(1) {
-        border: 1px solid black;
-        display: block;
-        flex-grow: 0;
-        flex-shrink: 1;
-        flex-basis: auto;
-        align-self: auto;
-        order: 0;
-    }
+        .w-50{
+            width: 50%;
+        }
+        .w-95{
+            width: 95%;
+        }
+        .mx-auto{
+            margin: 1rem auto;
+        }
+    </style>
 
-    .flex-items:nth-child(2) {
-        border: 1px solid black;
-        display: block;
-        flex-grow: 0;
-        flex-shrink: 1;
-        flex-basis: auto;
-        align-self: auto;
-        order: 0;
-    }
-
-    .flex-items:nth-child(3) {
-        border: 1px solid black;
-        display: block;
-        flex-grow: 0;
-        flex-shrink: 1;
-        flex-basis: auto;
-        align-self: auto;
-        order: 0;
-    }
-
-    .flex-items:nth-child(4) {
-        border: 1px solid black;
-        display: block;
-        flex-grow: 0;
-        flex-shrink: 1;
-        flex-basis: auto;
-        align-self: auto;
-        order: 0;
-    }
-</style>
-
-<body class="bg-gray-100">
-    <div class="titulo">
-        <h1 class="titulo_h">Historia clinica</h1>
-    </div>
-    <div class="parent">
-        <div class="div1"> {{ $formulario->created_at }} </div>
-        <div class="div2">
-            <div class="flex-container">
-                <div class="flex-items"></div>
-                <div class="flex-items"></div>
-                <div class="flex-items"></div>
-                <div class="flex-items"></div>
+    <body>
+        <div class="padre">
+            <h1 class="title">Ficha Médica</h1>
+            <div class="fecha">
+                <p class="fecha_contenido">{{ $formulario->created_at }}1</p>
             </div>
 
+
+            <div class="paciente text-left flex-col">
+                <div class="profesional flex-row w-50 mx-auto">
+                    <span class="titulo">Profesional</span>
+                    <span class="contenido"> {{ $formulario->nombre_profesional }}</span>
+                </div>
+                <div class="separador"></div>
+                <div class="flex-row w-95 mx-auto">
+                    <span class="titulo">Nombre del Paciente</span>
+                    <span class="contenido"> {{ $formulario->nombre_paciente }}</span>
+                </div>
+                <div class="separador"></div>
+                <div class="">
+                    <div class=" flex-row w-95 mx-auto">
+                        <span class="titulo">Obra Social</span>
+                        <span class="contenido">{{$formulario->obra_social}}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Nro Afiliado</span>
+                        <span class="contenido"></span>
+                    </div>
+                </div>
+                <div class="separador"></div>
+                <div class="">
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Domicilio</span>
+                        <span class="contenido"> {{ $formulario->domicilio }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Localidad</span>
+                        <span class="contenido"> {{ $formulario->localidad }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Código Postal</span>
+                        <span class="contenido"></span>
+                    </div>
+                </div>
+                <div class="separador"></div>
+                <div class="">
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Teléfono Particular</span>
+                        <span class="contenido"> {{ $formulario->telefono_particular }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Teléfono Celular</span>
+                        <span class="contenido"> {{ $formulario->telefono_celular }}</span>
+                    </div>
+                </div>
+                <div class="separador"></div>
+                <div class="">
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Fecha de Nacimiento</span>
+                        <span class="contenido"> {{ $formulario->fecha_nacimiento }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Edad</span>
+                        <span class="contenido"> {{ $formulario->edad }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">DNI</span>
+                        <span class="contenido"> {{ $formulario->dni }}</span>
+                    </div>
+                </div>
+                <div class="separador"></div>
+                <div class="">
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Profesional Derivante</span>
+                        <span class="contenido">{{ $formulario->profesional_derivantre }}</span>
+                    </div>
+                    <div class="flex-row w-95 mx-auto">
+                        <span class="titulo">Matricula</span>
+                        <span class="contenido">{{ $formulario->mat }}</span>
+                    </div>
+                </div>
+                <div class="separador"></div>
+                <div class="flex-row w-95 mx-auto">
+                    <span class="titulo">Profesión/Ocupación</span>
+                    <span class="contenido">{{ $formulario->profesion_ocupacion }}</span>
+                </div>
+            </div>
+            <div class="separador"></div>
+
+            <div class="antecedentes text-left">
+                <H2>Antecedentes Personales</H2>
+                <div class="separador"></div>
+                <div class="contenido"></div>
+            </div>
+
+            <div class="antecedentes text-left">
+                <h2>Antecedentes Familiares</h2>
+                <div class="separador"></div>
+                <div class="contenido">{{ $formulario->antecedentes_familiares }}</div>
+            </div>
+
+            <div class="antecedentes text-left">
+                <h2>Diagnóstico del Ingreso</h2>
+                <div class="separador"></div>
+                <div class="contenido">{{ $formulario->diagnostico_ingreso }}</div>
+            </div>
+
+            <div class="antecedentes text-left">
+                <H2>Evaluación Kinésica</H2>
+                <div class="separador"></div>
+                <div class="contenido"></div>
+            </div>
+
+            <div class="antecedentes text-left">
+                <h2>Plan de Tratamiento</h2>
+                <div class="separador"></div>
+                <div class="contenido">{{ $formulario->plan_tratamiento }}</div>
+            </div>
+
+            <div class="antecedentes text-left">
+                <h2>Objetivos del Tratamiento</h2>
+                <div class="separador"></div>
+                <div class="contenido"> {{ $formulario->objetivos_tratamiento }}</div>
+            </div>
         </div>
-    </div>
 
-    <p><strong>Nombre del profesional:</strong> {{ $formulario->nombre_profesional }}</p>
-    <p><strong>Nombre del paciente:</strong> {{ $formulario->nombre_paciente }}</p>
-    <p><strong>Edad:</strong> {{ $formulario->edad }}</p>
-    <p><strong>Obra social:</strong> {{ $formulario->obra_social }}</p>
-    <p><strong>Nro Carnet:</strong></p>
-    <p><strong>Domicilio:</strong> {{ $formulario->domicilio }}</p>
-    <p><strong></strong> {{ $formulario->localidad }}</p>
-    <p><strong>Teléfono particular:</strong> {{ $formulario->telefono_particular }}</p>
-    <p><strong>Teléfono celular:</strong> {{ $formulario->telefono_celular }}</p>
-    <p><strong>Fecha de Nacimiento:</strong> {{ $formulario->fecha_nacimiento }} , </p>
-    <p><strong>Edad</strong> {{ $formulario->edad }}</p>
-    <p><strong>DNI:</strong> {{ $formulario->dni }}</p>
-    <p><strong>Profesional Derivante:</strong> {{ $formulario->profesional_derivantre }}</p>
-    <p><strong>Matricula:</strong> {{ $formulario->mat }}</p>
-    <p><strong>Profesión:</strong> {{ $formulario->profesion_ocupacion }}</p>
-
-    <p><strong>Antecedentes Familiares:</strong> {{ $formulario->antecedentes_familiares }}</p>
-    <p><strong>Diagnóstico de Ingreso:</strong> {{ $formulario->diagnostico_ingreso }}</p>
-    <p><strong>Evaluación Kinesica</strong></p>
-    <p><strong>Plan de Tratamiento:</strong> {{ $formulario->plan_tratamiento }}</p>
-    <p><strong>Objetivos del Tratamiento:</strong> {{ $formulario->objetivos_tratamiento }}</p>
-    <div class="firmas">
-        <p><strong>Firma del paciente:</strong> {{ $formulario->fecha_nacimiento }}</p>
-        <p><strong>Fecha del profesional :</strong> {{ $formulario->fecha_nacimiento }}</p>
-
-    </div>
-
-</body>
+    </body>
 
 </html>
