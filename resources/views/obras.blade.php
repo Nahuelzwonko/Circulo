3bec661dfc5a10b4fc3514a2e5a1e138efbecb14
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2024-03-13 04:03:46 -0300

    cambios de obras

--- resources/views/obras.blade.php
+++ resources/views/obras.blade.php
@@ -5,18 +5,20 @@
 @section('Boton', '')
 @section('content')
 
-    <div class="py-16 mt-40">
+     <div class="py-16 mt-40">
         <div class="mx-auto px-6 max-w-6xl text-gray-500">
             
-            <div class=" grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
-               @foreach ($datos as $dato)
-                <div class="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 flex items-center justify-center" style="margin-right: 10px; margin-bottom: 10px;">
-                       <div class="relative">
+            <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                @php
+                    $datosOrdenados = $datos->sortByDesc('created_at');
+                @endphp
+                @foreach ($datosOrdenados as $dato)
+                    <div class="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 flex items-center justify-center" style="margin-right: 10px; margin-bottom: 10px;">
+                        <div class="relative">
                             <img src="{{ asset($dato->imagen) }}" alt="ImagenDeCKFM">
                         </div>
-                </div>
+                    </div>
                 @endforeach
-
             </div>
         </div>
     </div>
