beb29f743576f8cd0b4b7971c974638d99f9a582
Nahuel Zwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2024-03-13 03:33:41 -0300

    cambios de obras

--- resources/views/obras.blade.php
+++ resources/views/obras.blade.php
@@ -4,7 +4,7 @@
 @section('cont', '')
 @section('Boton', '')
 @section('content')
-    <div class=" mt-5 lg:mt-40 w-full">
+    <!-- <div class=" mt-5 lg:mt-40 w-full">
         <div class="grid items-center grid-cols-1 md:grid-cols-4  gap-1 w-auto h-auto  ">
             @foreach ($datos as $dato)
                 <div class="flex  items-center h-[300px] w-[300px] ">
@@ -13,7 +13,26 @@
             @endforeach
 
         </div>
+    </div> -->
+    <!-- component -->
+<section>
+    <div class="py-16">
+        <div class="mx-auto px-6 max-w-6xl text-gray-500">
+            <div class="text-center">
+                <h2 class="text-3xl text-gray-950 dark:text-white font-semibold">Quickstart with boilerplates</h2>
+                <p class="mt-6 text-gray-700 dark:text-gray-300">Harum quae dolore inventore repudiandae? orrupti aut temporibus ariatur.</p>
+            </div>
+            <div class="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                <div class="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 flex items-center justify-center ">
+                    <div aria-hidden="true" class="inset-0 absolute aspect-video border rounded-full -translate-y-1/2  to-white dark:from-white dark:to-white blur-2xl opacity-25 "></div>
+                    <div class="relative">
+                        <img src="https://kinesiologosmza.com/storage/imagenes/vYJZHxebEF5hGOBNFJFnRaLUYOY2S9HHYtgaAdYr.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</section>
     @include('include.cta')
     @include('include.footer')
 @endsection
